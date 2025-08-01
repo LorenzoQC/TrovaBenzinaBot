85985f82d948da9cbe1b352a21240c3cc101fa93
LorenzoQC <lorenzocucci02@gmail.com>
2025-08-01 14:57:54 +0200

    change /find messages

--- src/trovabenzina/handlers/find.py
+++ src/trovabenzina/handlers/find.py
@@ -129,7 +129,7 @@ async def run_search(origin, ctx: ContextTypes.DEFAULT_TYPE):
                 f"{medals[i]} <b><a href=\"{link}\">{station['brand']} • {station['name']}</a></b>\n"
                 f"<b>{t('address', lang)}</b>: {station['address']}\n"
                 f"<b>{t('price', lang)}</b>: {price:.3f} €xL\n"
-                f"<b>{t('saving', lang)}</b>: {abs(pct)}% ({t('average', lang)}: {avg:.3f} €xL)\n"
+                f"<b>{t('saving', lang)}</b>: {abs(pct)}% ({t('average', lang)}: {avg:.3f} €xL)"
             )
 
         await origin.message.reply_text(
