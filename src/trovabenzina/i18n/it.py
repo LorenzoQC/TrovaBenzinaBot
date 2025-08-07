9daed7bb276df6d0c692d3683ca524243bc98ae4
LorenzoQC <lorenzocucci02@gmail.com>
2025-08-07 15:58:41 +0200

    update /statistics labels

--- src/trovabenzina/i18n/it.py
+++ src/trovabenzina/i18n/it.py
@@ -61,11 +61,12 @@ TRANSLATIONS = {
         "<b>{num_searches} ricerche</b> effettuate.\n"
         "<b>{num_stations} distributori</b> analizzati.\n"
         "Risparmio medio: <b>{avg_eur_save_per_unit} {price_unit}</b>, ovvero il {avg_pct_save}%.\n"
-        "Risparmio annuo stimato: <b>{estimated_annual_save_eur}</b>!\n\n"
-        "<i>ℹ️ Come abbiamo calcolato questi dati?</i>\n"
-        "• Il risparmio medio è calcolato ipotizzando di effettuare sempre il rifornimento presso il distributore più economico proposto dal bot, confrontandone il prezzo con il prezzo medio della zona.\n"
-        "• Il risparmio annuo è calcolato ipotizzando una percorrenza di 10.000km annui con un consumo medio di {avg_consumption_per_100km}{uom} ogni 100km."
+        "Risparmio annuo stimato: <b>{estimated_annual_save_eur}</b>!"
     ),
+    "statistics_info": "<i>ℹ️ Come abbiamo calcolato questi dati?</i>\n"
+                       "• Il risparmio medio è calcolato ipotizzando di effettuare sempre il rifornimento presso il distributore più economico proposto dal bot, confrontandone il prezzo con il prezzo medio della zona.\n"
+                       "• Il risparmio annuo è calcolato ipotizzando una percorrenza di 10.000km annui con un consumo medio di: \n",
+    "fuel_consumption": " - {avg_consumption_per_100km}{uom} ogni 100km",
     "reset_statistics": "Azzera le statistiche ♻️",
     "statistics reset": "✅ Statistiche resettate correttamente!\n\nUtilizza il comando /search per avviare una ricerca.",
 
